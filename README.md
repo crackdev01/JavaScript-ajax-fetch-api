b6af206a1e4363dbf829ab2961cb2f85ac552ac6
crackdev08 <blackbox0823@gmail.com>
2019-05-07 14:03:25 +0200

    Working With Ajax & JSON

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # js_ajax_fetch_api_practice
+
+Started Coding: December 2018.
+
+Udemy course, practice with JavaScript AJAX, Fetch, API. 
+
+1. Lesson 1:
+XHR Object Methods & Working With Text.
+Files: index.v1 + app.v1.js:
+
+
+2. Lesson 2:
+Working With Ajax & JSON.
+Files: index.v2 + app.v2.js + customer.json + customers.json
+
+3. Lesson 3:
+Data From an External API - Chuck Norris Project.
+
+4. Lesson 4:
+Rest APIs & HTTP Requests.
+
+5. Lesson 5:
+Callback Functions.
+
+6. Lesson 6:
+Custom HTTP Library (Ajax With Callbacks).
+
+7. Lesson 7:
+ES6 Promises.
+
+8. Lesson 8:
+The Fetch API.
+
+9. Lesson 9:
+Arrow Functions.
+
+10. Lesson 10:
+Custom HTTP Library (Fetch With Promises).
+
+11. Lesson 11:
+Async & Await.
+
+12. Lesson 12:
+Custom HTTP Library (Fetch With Async Await).
+
+
+
--- app.v2.js
+++ app.v2.js
@@ -1,8 +1,12 @@
-//Create event listener for button1: 
+//Create event listener for button1:
 document.getElementById('button1').addEventListener('click', loadCustomer);
 
+//Create event listener for button2: 
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+// Load Single Customer (customer.json file):
 function loadCustomer(e) {
-    //Create a xmr request
+    //Create a xhr request
     const xhr = new XMLHttpRequest();
 
     // call OPEN, GET request, to customer.json and true for asynchronize
@@ -27,6 +31,48 @@ function loadCustomer(e) {
             `;
 
             document.getElementById('customer').innerHTML = output;
+        }
+    }
+
+    // send at the bottom to show the data
+    xhr.send();
+}
+
+// Load Customers (customers.json file):
+function loadCustomers(e) {
+    //Create a xhr request
+    const xhr = new XMLHttpRequest();
+
+    // call OPEN, GET request, to customer.json and true for asynchronize
+    xhr.open('GET', 'customers.json', true);
+
+    //ONLOAD
+    xhr.onload = function(){
+        //check to see if it is a 200 status
+        if(this.status === 200) {
+            //console.log(this.responseText);
+            //Show the output on the HTML:
+            // first we need to parse it as an object else it will be readed as a string and we won't be able to get customer.id or customer.name:
+            const customers = JSON.parse(this.responseText);
+
+            //Because it is an array, we can't output like above, so we need to loop through it with a forEach loop, but first we initiate and variable with output:
+
+            let output = '';
+            //we using let because we are going to reassign it within the loop below
+
+            customers.forEach(function(customer){
+                // we want to append on to output so we need to use +=
+                output += `
+                <ul>
+                    <li>ID: ${customer.id}</li>
+                    <li>Name: ${customer.name}</li>
+                    <li>Company: ${customer.company}</li>
+                    <li>Phone: ${customer.phone}</li>
+                </ul>
+            `;
+            });
+
+            document.getElementById('customers').innerHTML = output;
         }
     }
 
